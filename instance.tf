5e5b21e8b6e533ccd1c204ef443fd43430f761fa
gbalaji08 <36528624+gbalaji08@users.noreply.github.com>
2023-10-15 18:42:48 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   profile = "default"
   region  = "eu-north-1"
   access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 resource "aws_instance" "web" {
   ami           = "ami-02dc8ad50da58fffd"
