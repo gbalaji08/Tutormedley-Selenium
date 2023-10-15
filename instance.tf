ff466d6dab812f927740adcc08f4edef3383ade9
gbalaji08 <36528624+gbalaji08@users.noreply.github.com>
2023-10-15 19:06:50 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,26 @@
 provider "aws" {
-  region  = "eu-north-1"
+  region  = "us-east-1"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 resource "aws_instance" "web" {
-  ami           = "ami-0743788e185274ba7"
-  instance_type = "t3.micro"
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
   tags = {
 	Name = "Hello-GBH-Test"
 }
